7c8fa60199f9ba77a82584f7a29d2b97c89d9c09
Marek Walica <mwalica@gmail.com>
2026-02-25 07:43:32 +0100

    Initial Commit

---
+++ event_counter/event_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_counterApp.swift
+//  event_counter
+//
+//  Created by Marek Walica on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct event_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
